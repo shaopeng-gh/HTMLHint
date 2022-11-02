c177985daf62e8eb771dd8eaab18af8e83d690b6
Shaopeng Li <v-shaopengli@microsoft.com>
2022-11-02 01:17:07 -0700

    exclude macOS

--- test/cli/formatters/sarif.spec.js
+++ test/cli/formatters/sarif.spec.js
@@ -2,64 +2,67 @@ const ChildProcess = require('child_process')
 const fs = require('fs')
 const path = require('path')
 
-describe('CLI', () => {
-  describe('Formatter: sarif', () => {
-    it('should have stdout output with formatter sarif', (done) => {
-      const expectedFileContent = fs
-        .readFileSync(path.resolve(__dirname, 'sarif.sarif'), 'utf8')
-        .replace(
-          /\{\{path\}\}/g,
-          path.resolve(__dirname, 'example.html').replace(/\\/g, '\\\\')
-        )
-
-      const expected = JSON.parse(expectedFileContent)
-
-      var child = ChildProcess.spawn('node', [
-        path.resolve(__dirname, '../../../bin/htmlhint'),
-        path.resolve(__dirname, 'example.html'),
-        '--format',
-        'sarif',
-      ])
-
-      child.stdout.on('data', function (stdout) {
-        expect(stdout).not.toBe('')
-
-        const jsonStdout = JSON.parse(stdout)
-        expect(typeof jsonStdout).toBe('object')
-        expect(
-          jsonStdout['runs'][0]['artifacts'][0]['location']['uri']
-        ).toContain('example.html')
-
-        const stdoutResults = jsonStdout['runs'][0]['results']
-        const stdoutRules = jsonStdout['runs'][0]['tool']['driver']['rules']
-
-        expect(stdoutResults).toBeInstanceOf(Array)
-        expect(stdoutResults.length).toBe(expected['runs'][0]['results'].length)
-
-        expect(stdoutRules).toBeInstanceOf(Array)
-        expect(stdoutRules.length).toBe(
-          expected['runs'][0]['tool']['driver']['rules'].length
-        )
-
-        for (let i = 0; i < stdoutResults.length; i++) {
-          expect(stdoutResults[i]).toEqual(expected['runs'][0]['results'][i])
-        }
-
-        for (let i = 0; i < stdoutRules.length; i++) {
-          expect(stdoutRules[i]).toEqual(
-            expected['runs'][0]['tool']['driver']['rules'][i]
+if (os.platform() !== 'darwin')
+  describe('CLI', () => {
+    describe('Formatter: sarif', () => {
+      it('should have stdout output with formatter sarif', (done) => {
+        const expectedFileContent = fs
+          .readFileSync(path.resolve(__dirname, 'sarif.sarif'), 'utf8')
+          .replace(
+            /\{\{path\}\}/g,
+            path.resolve(__dirname, 'example.html').replace(/\\/g, '\\\\')
           )
-        }
-      })
 
-      child.stderr.on('data', function (stderr) {
-        expect(stderr).toBe('')
-      })
+        const expected = JSON.parse(expectedFileContent)
 
-      child.on('close', function (code) {
-        expect(code).toBe(1)
-        done()
+        var child = ChildProcess.spawn('node', [
+          path.resolve(__dirname, '../../../bin/htmlhint'),
+          path.resolve(__dirname, 'example.html'),
+          '--format',
+          'sarif',
+        ])
+
+        child.stdout.on('data', function (stdout) {
+          expect(stdout).not.toBe('')
+
+          const jsonStdout = JSON.parse(stdout)
+          expect(typeof jsonStdout).toBe('object')
+          expect(
+            jsonStdout['runs'][0]['artifacts'][0]['location']['uri']
+          ).toContain('example.html')
+
+          const stdoutResults = jsonStdout['runs'][0]['results']
+          const stdoutRules = jsonStdout['runs'][0]['tool']['driver']['rules']
+
+          expect(stdoutResults).toBeInstanceOf(Array)
+          expect(stdoutResults.length).toBe(
+            expected['runs'][0]['results'].length
+          )
+
+          expect(stdoutRules).toBeInstanceOf(Array)
+          expect(stdoutRules.length).toBe(
+            expected['runs'][0]['tool']['driver']['rules'].length
+          )
+
+          for (let i = 0; i < stdoutResults.length; i++) {
+            expect(stdoutResults[i]).toEqual(expected['runs'][0]['results'][i])
+          }
+
+          for (let i = 0; i < stdoutRules.length; i++) {
+            expect(stdoutRules[i]).toEqual(
+              expected['runs'][0]['tool']['driver']['rules'][i]
+            )
+          }
+        })
+
+        child.stderr.on('data', function (stderr) {
+          expect(stderr).toBe('')
+        })
+
+        child.on('close', function (code) {
+          expect(code).toBe(1)
+          done()
+        })
       })
     })
   })
-})
